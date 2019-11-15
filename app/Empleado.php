afc0b9a773fd8510143183baa5a76fe0341dcb16
cynda3 <andergonzalez33@gmail.com>
2019-11-15 08:13:27 +0100

    Relaciones hechas

--- app/Empleado.php
+++ app/Empleado.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado extends Model
 {
     protected $table = 'empleados';
+
+
+    public function proyecto()
+    {
+        return $this->hasOne('App\Proyecto');
+    }
+
+    
+    public function departamento()
+    {
+        return $this->hasOne('App\Departamento');
+    }
 }
--- app/Proyecto.php
+++ app/Proyecto.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Proyecto extends Model
 {
     protected $table = 'proyectos';
+
+
+    public function empleado()
+    {
+        return $this->belongsTo('App\Empleado');
+    }
+
+
 }
